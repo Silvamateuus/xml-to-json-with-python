cf619c4f2ecddc40062a16683f937dbb18d60a4e
silvamateuus <mateuus.s.silva@gmail.com>
2020-07-03 16:06:10 -0300

    Refactored code,  created folder to files json

--- converte_xml_json.py
+++ converte_xml_json.py
@@ -1,20 +1,37 @@
 import xmltodict
 import json
 import os
+import os.path
 
+
+if not os.path.exists('folder_json'):
+    
+    os.mkdir('folder_json')
+    
 funcionalidades = ['TESTS-login','TESTS-parametro_de _volatilidade_correlacao']
 
 for x in funcionalidades:
+    
     resultado = x
 
     with open(resultado + '.xml') as in_file:
+        
         xml = in_file.read()
-        with open(resultado + '.json', 'w') as out_file:
+        
+        with open('folder_json/'+ resultado + '.json', 'w') as out_file:
+        
             json.dump(xmltodict.parse(xml), out_file)
-        with open(resultado +'.json', 'r') as out_file:
+        
+        with open('folder_json/' + resultado +'.json', 'r') as out_file:
+        
+            dir = './json' 
+        
             data = json.load(out_file)
+
     j = (data['testsuite'])
     
+
+    # Exibição dos reports das features através do arquivo json gerado
     print ('|--------------------------')
 
     nome = j['@name']
